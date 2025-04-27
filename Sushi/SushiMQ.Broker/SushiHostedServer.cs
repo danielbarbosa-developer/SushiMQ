6da22a45c532cff4d29717540022b3e5ea14b035
danielbarbosa-developer <69679521+danielbarbosa-developer@users.noreply.github.com>
2025-04-27 19:21:16 -0300

    Add message function in ReadOnceEngine.cs and benchmark

--- Sushi/SushiMQ.Broker/SushiHostedServer.cs
+++ Sushi/SushiMQ.Broker/SushiHostedServer.cs
@@ -1,19 +1,23 @@
 using Microsoft.Extensions.Hosting;
 using SushiMQ.Broker.Interfaces;
+using SushiMQ.Engine;
 
 namespace SushiMQ.Broker;
 
 public class SushiHostedServer : IHostedService
 {
     private ISushiListener _listener;
+    private ISushiEngine _engine;
     
-    public SushiHostedServer(ISushiListener listener)
+    public SushiHostedServer(ISushiListener listener, ISushiEngine engine)
     {
         _listener = listener;
+        _engine = engine;
     }
     
     public Task StartAsync(CancellationToken cancellationToken)
     {
+        _engine.Start();
         _listener.Start();
         return Task.CompletedTask;
     }
